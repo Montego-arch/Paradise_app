ddb9df515a60fc4a48b6d4d05bcd7b39273df73b
Montego-arch <mmanuelmiles@gmail.com>
2023-10-06 10:47:32 +0100

    erp next tasks

--- paradise_app/hooks.py
+++ paradise_app/hooks.py
@@ -98,13 +98,15 @@ doctype_js = {
 # Hook on document methods and events
 
 doc_events = {
-	"*": {
-		"on_update": "method",
-		"on_cancel": "method",
-		"on_trash": "method"
-	},
+	# "*": {
+	# 	"on_update": "method",
+	# 	"on_cancel": "method",
+	# 	"on_trash": "method"
+	# },
     "Property": {
-        "validate": "paradise_app.paradise_app.doctype.property.events.validate"
+        "validate": "paradise_app.paradise_app.doctype.property.events.validate",
+        "on_update": "paradise_app.paradise_app.doctype.property.events.on_update",
+        "after_insert": "paradise_app.paradise_app.doctype.property.events.after_insert"
 	}
 }
 
