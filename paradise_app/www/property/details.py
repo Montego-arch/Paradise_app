37439ddac80cf10c570800718aeeaca805dd3188
Montego-arch <mmanuelmiles@gmail.com>
2023-10-10 17:39:25 +0100

    currency formatting

--- paradise_app/www/property/details.py
+++ paradise_app/www/property/details.py
@@ -7,6 +7,13 @@ def get_context(context):
         docname = frappe.form_dict.docname
         context.property = frappe.get_doc("Property", frappe.form_dict.docname)
         context.agent = frappe.get_doc("Agent", context.property.agent)
+        related_properties = frappe.db.sql(f"""
+            SELECT creation, name, property_name, status, address, grand_total,
+            image FROM `tabProperty` WHERE property_type='{context.property.property_type}'
+            AND name != '{context.property.name}' ORDER BY creation DESC LIMIT 3;
+            """, as_dict=True)
+        context.related_properties = related_properties
+       
 
     except Exception as e:
         frappe.local.flags.redirect_location = '/404'
