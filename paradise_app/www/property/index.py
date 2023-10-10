ef25a0a7106eea912827f9671a070f57fc449477
Montego-arch <mmanuelmiles@gmail.com>
2023-10-10 16:57:41 +0100

    new commit

--- paradise_app/www/property/index.py
+++ paradise_app/www/property/index.py
@@ -2,9 +2,9 @@ import frappe
 
 def get_context(context):
 
-    properties = frappe.db.sqldocs = frappe.db.sql("""
-                                                   SELECT name, property_name, status, address, grand_total, image FROM `tabProperty`
-                                                   ;""", as_dict=True)
-    context.property = properties
+    properties = frappe.db.sql("""
+        SELECT name, property_name, status, address, grand_total, image FROM `tabProperty` ORDER BY creation DESC;
+        """, as_dict=True)
+    context.properties = properties
 
     return context
